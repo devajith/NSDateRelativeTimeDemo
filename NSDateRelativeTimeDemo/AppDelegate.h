a17e445fb40d43dc871a16e8e54b86ede5274015
devajith <dev.ajith89@gmail.com>
2015-08-09 22:51:24 +0530

    Initial Commit

---
+++ NSDateRelativeTimeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSDateRelativeTimeDemo
+//
+//  Created by Ajith Kumar on 09/08/15.
+//  Copyright (c) 2015 Ajith Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NSDateRelativeTimeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSDateRelativeTimeDemo
+//
+//  Created by Ajith Kumar on 09/08/15.
+//  Copyright (c) 2015 Ajith Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
